bef0fd5ffda116f3c3deec3277ef50753ecf78dd
Ajay Singadiya <ajay.singadiya@VLINK.IN>
2018-07-26 14:41:29 +0530

    removed add from master

--- master.java
+++ master.java
@@ -4,15 +4,5 @@ class Master{
 		int a =10;
 		int b= 5;
 	}
-	public static int add(int a, int b){
-		int result = a + b;
-		return result;
-	}
-
-	public static int add(int a, int b,int c){
-		int result = a + b + c;
-		return result;
-	}
-
 
 }
