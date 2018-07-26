b0a4b6a41b80cd92b763d282c46b45c926f3cc5d
Ajay Singadiya <ajay.singadiya@VLINK.IN>
2018-07-26 15:02:18 +0530

    called methods

--- master.java
+++ master.java
@@ -3,6 +3,10 @@ class Master{
 	{
 		int a =10;
 		int b= 5;
+		System.out.println(add(a,b));
+		System.out.println(sub(a,b));
+		System.out.println(mul(a,b));
+		System.out.println(div(a,b));
 	}
 
 	public static int add(int a, int b){
@@ -20,7 +24,7 @@ class Master{
 		return result;
 	}
 	public static long div(int a, int b){
-		int result = a / b;
+		long result = a / b;
 		return result;
 	}
 }
